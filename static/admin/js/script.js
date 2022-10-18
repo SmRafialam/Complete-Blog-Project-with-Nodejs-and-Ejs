381715c6d16a3d433ce2e60dd9e4cd0f42a8a5aa
SmRafialam <smrafialam.com>
2022-10-18 16:55:13 +0600

    showing categories in posts

--- static/admin/js/script.js
+++ static/admin/js/script.js
@@ -6,8 +6,14 @@
         $('#multiple-checkboxes').multiselect({
           includeSelectAllOption: true,
         });
-        $(".table").DataTable({
-          order: [0, 'desc']
-        });
+        // $("#table").DataTable({
+        //   order: [0, 'desc']
+        // });
+        
     });
 })(jQuery);
+
+tinymce.init({
+  selector: 'textarea#my-expressjs-tinymce-app',
+  plugins: 'lists link image table code help wordcount'
+});
